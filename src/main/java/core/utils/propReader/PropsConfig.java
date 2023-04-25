11fd677fc86a29f71b27059f8dcd8f6c71d1fb32
Sergey Potapov <sergeypotapoff@12gmail.com>
2023-04-25 13:34:05 +0300

    was added Owner properties and deleted Prop reader class

---
+++ src/main/java/core/utils/propReader/PropsConfig.java
@@ -0,0 +1,21 @@
+package core.utils.propReader;
+
+
+import org.aeonbits.owner.Config;
+
+/**
+ * Following interface specifies the policy for loading the properties files.
+ * By default the first available properties file specified by Config.Sources.
+ */
+@Config.Sources({"classpath:runnerConfig.properties"})
+public interface PropsConfig extends Config {
+
+    @Config.Key("base.browser")
+    String BASE_BROWSER();
+
+    @Config.Key("base.url")
+    String BASE_URL();
+
+    @Config.Key("waiting.timeout")
+    String WAITING_TIMEOUT();
+}
